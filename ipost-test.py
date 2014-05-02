543eb8a92e6509aa18e70cf3ca69c34978124f2a
lnunez <lnunez@c3isecurity.com>
2014-05-01 20:22:10 -0400

    update ipostlib.py to be a module. update ipost-test.py to be self test.
    	renamed:    ipostlib.py -> ipost-test.py
    	modified:   ipostlib.py

--- ipost-test.py
+++ ipost-test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """
+ Ipost-test.py is a self test of the functions.
  This script explores and demonstrates the Risk Scoring algorithms and formulas used in the 
  Department of State (DoS) iPost program.
  The examples below are based on the US Department of State Case Study found at 
@@ -11,20 +12,21 @@
  The ending total score could be used to aggregate a site, and enterprise score.
 """
 
-# This python module is for the iPost Risk Scoring forumlas.
-
-
 # The math module is need for some of the formulas.
 import math
 
-__author__ = "Luis NUnez"
-__copyright__ = "Copyright 2014, C3isecurity"
+# Variables
+# Adjust the AV_Age and CVSS scores to see results.
+AV_Age = 70
+CVSS = [10.0, 9.0, 8, 7, 6, 5, 4, 3, 2, 1]
+Host_Vul_Score = 0
+Count_Vuls = 0
+#PAT_Missing []
 
-__license__ = "GPLv3"
-__version__ = "0.1"
-__maintainer__ = "Luis Nunez"
-__email__ = "lnunez@c3isecurity.com"
-__status__ = "Prototype"
+
+# CVSS Severity Levels
+High = 7
+Low = 3
 
 
 # Functions: Formula 
@@ -45,5 +47,37 @@ def Host_PAT_Score ():
 	Host_PAT_Score += Host_PAT_Score
 	print test
 
-if __name__ == '__main__':
-	print "ipostlib"
+
+# Start of main body of script
+
+
+# This route reads the CVSS list as vulnerability score to process
+# it then gets the total 
+print "CVSS\tDoS"
+for i in CVSS:
+	int(i)
+	print "%d\t\t%d " % (i,  Vul_Score(i))
+	Host_Vul_Score += Vul_Score(i)
+	if Host_Vul_Score >= High: 	# Higher or equal to 7
+		Severity = "High"
+	elif Host_Vul_Score < High and Host_Vul_Score > Low:
+		Severity = "Medium" 	# Less the 7 but higher than 3
+	elif Host_Vul_Score <= Low: # less than or equal to 3
+		Severity = "Low"
+	Count_Vuls += 1
+
+
+
+# Report section
+
+# Print to standard out.
+
+print "Start Self Test of Script"
+print "iPost Results\n"
+print "Number Vuls calculated:", Count_Vuls
+#print "Host Vulnerability  Score:\t\t\t", Vul_Score(10)
+print "Host_AVR_Score:\t\t\t", Host_AVR_Score(3.1)
+print "Total Host Vul Score:\t", Host_Vul_Score
+print "Severity:\t\t\t\t", Severity
+
+print "\nend of script"
