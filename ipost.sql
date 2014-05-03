89f5a4bcfd6c8d96fd7b840d4264e9f8ad328c1e
lnunez <lnunez@c3isecurity.com>
2014-05-03 12:06:23 -0400

    add sqlscript to create new sqlite datebase. The script inserts tables and test data for the ipost scripts.
    	new file:   ipost.sql

---
+++ ipost.sql
@@ -0,0 +1,17 @@
+/* SQL script to create and insert sample ipost.db */
+/* To run the script:
+/* $ sqlite3 ipost.db < ipost.sql */
+
+CREATE TABLE results(date,hostname,score);
+CREATE TABLE vuls
+			(date text, title text, CVE text, cvss real, ipost real);
+
+
+/* Single insert */
+INSERT INTO vuls VALUES ('2014-04-28','MS Office File Format Converter Vulnerability', 'CVE-2014-1757',9.3,0.0);
+
+/* Bulk insert */
+INSERT INTO "vuls" VALUES('2014-04-28','MS Office File Format Converter Vulnerability','CVE-2014-1757',9.3,0.0);
+INSERT INTO "vuls" VALUES('2013-04-28','Microsoft Word Stack Overflow Vulnerability','CVE-2014-1758',9.3,0.0);
+INSERT INTO "vuls" VALUES('2013-04-28','Word RTF Memory Corruption Vulnerability','CVE-2014-1761',9.3,0.0);
+INSERT INTO "vuls" VALUES('2013-04-28','Internet Explorer Memory Corruption Vulnerability ','CVE-2014-0235',9.3,0.0);
